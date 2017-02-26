bf94a43fc2e8a759d1f130e9259a658738aed3ba
Genert <orggenert@gmail.com>
2017-02-26 20:48:43 +0200

    Add a SI unit test.

---
+++ Tests/SIUnits.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Archetypes.SystemOfUnits.SI.Units;
+
+namespace Tests {
+    [TestClass]
+    public class SiUnits {
+        [TestMethod]
+        public void Kelvin() {
+            Kelvin unit = new Kelvin();
+
+            Assert.AreEqual("K", unit.GetSymbol());
+            Assert.AreEqual("kelvin", unit.GetName());
+        }
+    }
+}
