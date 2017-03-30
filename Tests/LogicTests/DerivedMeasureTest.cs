f2a74755805d0a3ac8e1c04410e64177e189967d
SiimErik <SiimErik@OUNAKE>
2017-03-30 15:31:52 +0300

    Derivedeasur teemad

---
+++ Tests/LogicTests/DerivedMeasureTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Logic;
+namespace Tests.LogicTests
+{
+    [TestClass]
+    public class DerivedMeasureTest
+    {
+        private DerivedMeasure t;
+        [TestInitialize]
+        public void InitTests()
+        {
+            t = new DerivedMeasure();
+        }
+
+        [TestCleanup]
+        public void CleanTests()
+        {
+            t = null;
+        }
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            Assert.IsNotNull(t);
+        }
+
+    }
+}
