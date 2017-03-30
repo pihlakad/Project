31b5ae407f9bcefa193062f7c74d75250b107710
SiimErik <SiimErik@OUNAKE>
2017-03-30 15:00:27 +0300

    Midagi erilist

--- Logic/Metric.cs
+++ Logic/Metric.cs
@@ -3,24 +3,24 @@
 namespace Logic
 {
     public class Metric : Archetype {
-        private string _name;
-        private string _symbol;
-        private string _definition;
+        private string name;
+        private string symbol;
+        private string definition;
 
         public string Name
         {
-            get { return Strings.EmptyIfNull(_name); }
-            set { _name = value; }
+            get { return Strings.EmptyIfNull(name); }
+            set { name = value; }
         }
         public string Symbol {
-            get { return Strings.EmptyIfNull(_symbol); }
-            set { _symbol = value; }
+            get { return Strings.EmptyIfNull(symbol); }
+            set { symbol = value; }
         }
 
         public string Definition
         {
-            get { return Strings.EmptyIfNull(_definition); }
-            set { _definition = value; }
+            get { return Strings.EmptyIfNull(definition); }
+            set { definition = value; }
         }      
     }
 }
