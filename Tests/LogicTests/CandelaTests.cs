dd9f1a243c00210e6fef6c14d06fc36190053b08
SiimErik <SiimErik@OUNAKE>
2017-04-03 09:49:59 +0300

    Suurt midagi

---
+++ Tests/LogicTests/CandelaTests.cs
@@ -0,0 +1,33 @@
+﻿using Logic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.LogicTests
+{
+    [TestClass]
+    public class CandelaTests
+    {
+        private Candela c;
+
+        [TestInitialize]
+        public void InitTests()
+        {
+            c = new Candela();
+        }
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            Assert.IsNotNull(c);
+        }
+
+        [TestMethod]
+        public void NameTest()
+        {
+            Assert.AreEqual(c.Name, "candela");
+        }
+
+        [TestMethod]
+        public void SymbolTest() {
+            Assert.AreEqual(c.Symbol,"cd");
+        }
+    }
+}
---
+++ Tests/LogicTests/SecondTests.cs
@@ -0,0 +1,32 @@
+﻿using Logic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.LogicTests
+{
+    [TestClass]
+    public class SecondTests
+    {
+        private Second s;
+        [TestInitialize]
+        public void InitTests()
+        {
+            s = new Second();
+        }
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            Assert.IsNotNull(s);
+        }
+
+        [TestMethod]
+        public void NameTest()
+        {
+            Assert.AreEqual(s.Name, "second");
+        }
+        [TestMethod]
+        public void SymbolTest()
+        {
+            Assert.AreEqual(s.Symbol, "s");
+        }
+    }
+}
