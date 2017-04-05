2253f156b83b3f8b85a5a3a74f8721a99e63f87f
annuW <annuW@DESKTOP-8NDLHKQ>
2017-04-05 21:04:03 +0300

    Intensity and Substance

---
+++ Logic/SIBaseUnit/IntensityUnits.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.SIBaseUnit
+{
+  public  static class IntensityUnits
+    {
+        public static void Initialize() {
+            var m = Measures.Add("intensity");
+            Units.Add(m, 1, "cd", "candela");
+            ;
+
+        }
+    }
+}
