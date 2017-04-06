a88d6db10c2e3785243d37144b2e15aa8afc1b70
Evelin Veermets <evveer@ttu.ee>
2017-04-06 16:44:30 +0300

    Test1

---
+++ Tests/LogicTests/SIBaseUnitTests/MassUnitsTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.LogicTests.SIBaseUnitTests
+{
+    [TestClass]
+    public class MassUnitsTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
