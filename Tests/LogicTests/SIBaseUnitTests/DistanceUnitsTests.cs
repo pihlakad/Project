c03ea4a465b1264f90b0d60dfc39b21e64b9c032
SiimErik <SiimErik@OUNAKE>
2017-04-06 15:42:23 +0300

    tegin ühe testi. Ei tea kas õigesti

--- Tests/LogicTests/SIBaseUnitTests/DistanceUnitsTests.cs
+++ Tests/LogicTests/SIBaseUnitTests/DistanceUnitsTests.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using Logic;
+﻿using Logic;
+using Logic.SIBaseUnit;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Tests.LogicTests.SIBaseUnitTests
@@ -7,8 +7,16 @@ namespace Tests.LogicTests.SIBaseUnitTests
     [TestClass]
     public class DistanceUnitsTests {
         [TestMethod]
-        public void TestMethod1()
-        {
+        public void InitializeTest() {
+            var m = Measures.Instance.Find(x => x.Name =="distance");
+            if (m != null)
+                Measures.Instance.Remove(m);
+            var n = Measures.Instance.Find(x => x.Name == "distance");
+            Assert.IsNull(n);
+            DistanceUnits.Initialize();
+            n = Measures.Instance.Find(x => x.Name == "distance");
+            Assert.IsNotNull(n);
+            Assert.AreEqual("distance", n.Name);
         }
     }
 }
