9f96841185f48b31c27a7df5ed335b4288f3cec9
annuW <annuW@DESKTOP-8NDLHKQ>
2017-04-11 17:44:32 +0300

    midagi

--- Tests/LogicTests/SIBaseUnitTests/DistanceUnitsTests.cs
+++ Tests/LogicTests/SIBaseUnitTests/DistanceUnitsTests.cs
@@ -1,4 +1,5 @@
-﻿using Logic;
+﻿using System.Security.Cryptography.X509Certificates;
+using Logic;
 using Logic.SIBaseUnit;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -12,7 +13,12 @@ namespace Tests.LogicTests.SIBaseUnitTests
             Units.Instance.Clear();
             DistanceUnit.Initialize();
             Assert.AreEqual("distance", Measures.Instance.Find(x=>x.Name == "distance").UniqueId);
-            Assert.AreEqual("meter", Units.Instance.Find(x => x.Name == "meter").UniqueId);
+            Assert.AreEqual("kilomeeter",Units.Instance.Find(x => x.Name == "kilomeeter").UniqueId);
+            Assert.AreEqual("meeter", Units.Instance.Find(x => x.Name == "meeter").UniqueId);
+            Assert.AreEqual("detsimeeter", Units.Instance.Find(x => x.Name == "detsimeeter").UniqueId);
+            Assert.AreEqual("sentimeeter", Units.Instance.Find(x => x.Name == "sentimeeter").UniqueId);
+            Assert.AreEqual("millimeeter", Units.Instance.Find(x => x.Name == "millimeeter").UniqueId);
+
         }
     }
 }
