30a9f09e83c5bf443c229406fdae68a6166ce14c
Evelin Veermets <evveer@ttu.ee>
2017-04-12 23:13:46 +0300

    Derived measur loogika - funktsionaalsus vigane

--- Logic/Units.cs
+++ Logic/Units.cs
@@ -17,6 +17,15 @@ namespace Logic
             return u;
         }
 
+        internal static Unit Add(DerivedMeasure m, double factor, string symbol, string name = null)
+        {
+            var u = Instance.Find(x => x.Measure == m.UniqueId && x.Symbol == symbol);
+            if (u != null) return u;
+            u = new Unit(m, factor, symbol, name);
+            Instance.Add(u);
+            return u;
+        }
+
         internal static Unit Find(string unit)
         {
             return Instance.Find(x => x.UniqueId == unit);
