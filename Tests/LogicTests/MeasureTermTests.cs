f3973910b1fede54ae7cf86c9eee3dc33030b7ee
ellen <ellen@DESKTOP-CSVTCIT>
2017-04-24 17:12:26 +0300

    Paar testi

---
+++ Tests/LogicTests/MeasureTermTests.cs
@@ -0,0 +1,33 @@
+﻿using Logic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.LogicTests
+{
+    [TestClass]
+    public class MeasureTermTests: CommonTests<MeasureTerm>
+    
+    {
+        [TestMethod]
+
+        protected override MeasureTerm getRandomObj() {
+            return MeasureTerm.Random();
+        }
+
+        private MeasureTerm b;
+        [TestInitialize]
+        public void InitTests()
+        {
+            b = new MeasureTerm();
+        }
+        [TestCleanup]
+        public void CleanTests()
+        {
+            b = null;
+        }
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            Assert.IsNotNull(b);
+        }
+    }
+}
---
+++ Tests/LogicTests/UnitTermTests.cs
@@ -0,0 +1,33 @@
+﻿using Logic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace Tests.LogicTests
+{
+    [TestClass]
+    public class UnitTermTests : CommonTests<UnitTerm>
+
+    {
+        [TestMethod]
+
+        protected override UnitTerm getRandomObj()
+        {
+            return UnitTerm.Random();
+        }
+
+        private UnitTerm b;
+        [TestInitialize]
+        public void InitTests()
+        {
+            b = new UnitTerm();
+        }
+        [TestCleanup]
+        public void CleanTests()
+        {
+            b = null;
+        }
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            Assert.IsNotNull(b);
+        }
+    }
+}
