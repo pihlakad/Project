95968838ff63ae5d490427faac6eb54fe9f592e8
annuW <annuW@DESKTOP-8NDLHKQ>
2017-05-14 23:45:49 +0300

    Mingi kalkukas peaaegu valmis. Nipet näpet sellele veel

--- Software/ViewModel/HtmlLists.cs
+++ Software/ViewModel/HtmlLists.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Web;
 
@@ -10,5 +11,6 @@ namespace Software.ViewModel
         public static IEnumerable<string> Operations = new List<string> {
             "Add", "Subtract", "Multiply", "Divide", "Compare", "Round"
         };
+       
     }
-}
+  }
