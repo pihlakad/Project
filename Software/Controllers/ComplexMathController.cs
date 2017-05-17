5a884b9994f353cd06819e458160361cce9f56ba
Siim Erik Potsepp <siimpotsepp14@outlook.com>
2017-05-17 15:29:31 +0300

    Kaks asja valma

--- Software/Controllers/ComplexMathController.cs
+++ Software/Controllers/ComplexMathController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.EnterpriseServices;
 using System.Web.Mvc;
 using Logic;
 using Software.ViewModel;
@@ -17,9 +18,9 @@ namespace Software.Controllers
         public ActionResult Result(ComplexMathViewModel quantity) {
             var q1 = quantity.FirstQuantity;
             var q2 = quantity.SecondQuantity;
-            var baseMeasure = new BaseMeasure(quantity.Measure);
+            var baseMeasure = BaseMeasure.Random();
             Measures.Instance.Add(baseMeasure);
-            var derivedMeasure = baseMeasure.Multiply(baseMeasure);
+            var derivedMeasure = DerivedMeasure.Random();
             Measures.Instance.Add(derivedMeasure);    
             int i = 0;
             List<DerivedUnit> derivedUnits = new List<DerivedUnit>();
@@ -31,30 +32,44 @@ namespace Software.Controllers
                 Units.Instance.Add(derivedUnit);
                 derivedUnits.Add(derivedUnit);
                 i++;
+                if(i==quantity.NumberOfUnits) break;
             }            
-            for (int j = 0; j < 3; j++)
+            for (int j = 0; j < quantity.NumberOfUnits - 1; j++)
             {
                 var a = derivedUnits[j].Multiply(derivedUnits[j + 1]) as DerivedUnit;
                 Units.Instance.Add(a);
                 derivedUnits[j + 1] = a;
-                if (j == 2) {
+                if (j == quantity.NumberOfUnits - 2) {
                     q1.Unit = a.Name;
                     q2.Unit = a.Name;
                 }                                   
-            }            
-            
+            }
+            quantity = SetResults(quantity, q1, q2);                                           
+            return View("Index", quantity);
+        }
 
+        private ComplexMathViewModel SetResults(ComplexMathViewModel quantity, Quantity q1, Quantity q2) {
             if (quantity.SelectedOperation == "Add")
                 quantity.ResultQuantity = q1.Add(q2);
             else if (quantity.SelectedOperation == "Subtract")
                 quantity.ResultQuantity = q1.Subtract(q2);
             else if (quantity.SelectedOperation == "Multiply")
                 quantity.ResultQuantity = q1.Multiply(q2);
-            else quantity.ResultQuantity = q1.Divide(q2);            
-
-            return View("Index", quantity);
+            else if (quantity.SelectedOperation == "Divide")
+                quantity.ResultQuantity = q1.Divide(q2);
+            else if (quantity.SelectedOperation == "Compare")
+                quantity.Result = SetComparingResult(q1, q2);
+            else if (quantity.SelectedOperation == "Round")
+                quantity.ResultQuantity = q1.Round(new Rounding());
+            if (quantity.Result == null)
+                quantity.Result = quantity.ResultQuantity.Amount.ToString();
+            return quantity;
         }
 
-       
+        private string SetComparingResult(Quantity q1, Quantity q2) {
+            var b = q1.IsGreaterThan(q2);
+            if (b) return "Second Amount is Greater";
+            return "First Amount is Greater";
+        }
     }
 }
--- Software/ViewModel/MathViewModel.cs
+++ Software/ViewModel/MathViewModel.cs
@@ -8,8 +8,7 @@ namespace Software.ViewModel
 {
     public class MathViewModel {
         private List<Quantity> quantityList;
-        private Quantity resultQuantity = Quantity.Empty;
-        private string measure;
+        private Quantity resultQuantity = Quantity.Empty;        
         private string selectedOperation;
 
         public List<Quantity> QuantityList {
@@ -21,17 +20,11 @@ namespace Software.ViewModel
             get { return resultQuantity; }
             set { resultQuantity = value; }
         }
-
-        public string Measure
-        {
-            get { return measure; }
-            set { measure = value; }
-        }
-
+       
         public string SelectedOperation {
             get { return selectedOperation; }
             set { selectedOperation = value; }
         }       
-        public string ResultAmount { get; set; }        
+        public string Result { get; set; }        
     }
 }
